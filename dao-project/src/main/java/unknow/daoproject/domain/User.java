86a617d29ad0d464154f7210d1c149ed2ee89f30
chan <986632543@qq.com>
2019-01-08 14:44:30 +0800

    add dao

---
+++ dao-project/src/main/java/unknow/daoproject/domain/User.java
@@ -0,0 +1,33 @@
+package unknow.daoproject.domain;
+
+public class User {
+    private Integer id;
+
+    private String user;
+
+    private String pass;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user == null ? null : user.trim();
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass == null ? null : pass.trim();
+    }
+}
---
+++ dao-project/src/main/java/unknow/daoproject/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package unknow.daoproject.mapper;
+
+import unknow.daoproject.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
