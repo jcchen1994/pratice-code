9fa1e3182faf2af2bb12fcb27c7f9f51112c0fe3
chan <986632543@qq.com>
2019-01-11 01:07:16 +0800

    1

--- tool-project/src/main/java/unknow/toolproject/ToolProjectApplication.java
+++ tool-project/src/main/java/unknow/toolproject/ToolProjectApplication.java
@@ -1,5 +1,6 @@
 package unknow.toolproject;
 
+import redis.clients.jedis.Jedis;
 import unknow.toolproject.redis.RedisClient;
 
 /*
@@ -11,9 +12,16 @@ public class ToolProjectApplication {
 /*
     SpringApplication.run(ToolProjectApplication.class, args);
 */
-    RedisClient redisClient = RedisClient.getInstance("192.168.2.218",
-        6379,"1234567");
-    System.out.println(redisClient.get("uc"));
+    RedisClient redisClient = RedisClient.getInstance("10.211.55.11",
+        6379,"123456");
+    try (Jedis jedis = redisClient.getJedis()){
+      jedis.lpush("aa", "bb", "cc");
+      System.out.println(jedis.lpop("aa"));
+      System.out.println(jedis.lpop("aa"));
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
